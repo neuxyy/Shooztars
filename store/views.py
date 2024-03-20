522464b8142e59b824357f10bcb0be85975415c3
neuxyy <neuxyy@gmail.com>
2024-03-20 22:30:42 +0200

    paypal option added

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
-from . utils import cookieCart, cartData
+from . utils import cookieCart, cartData, guestOrder
 
 
 from .models import *
@@ -63,29 +63,32 @@ def updateItem(request):
         return JsonResponse('Item was added successfully', safe=False)
 
 
+
 def processOrder(request):
-	transaction_id = datetime.datetime.now().timestamp()
-	data = json.loads(request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		total = float(data['form']['total'])
-		order.transaction_id = transaction_id
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-		if total == order.get_cart_total:
-			order.complete = True
-		order.save()
+    else:
+        customer, order = guestOrder(request, data)
 
-		if order.shipping == True:
-			ShippingAddress.objects.create(
-			customer=customer,
-			order=order,
-			address=data['shipping']['address'],
-			city=data['shipping']['city'],
-			zipcode=data['shipping']['zipcode'],
-			)
-	else:
-		print('User is not logged in')
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
 
-	return JsonResponse('Payment submitted..', safe=False)
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+        customer=customer,
+        order=order,
+        address=data['shipping']['address'],
+        city=data['shipping']['city'],
+        zipcode=data['shipping']['zipcode'],
+        )
+        
+    return JsonResponse('Payment submitted..', safe=False)

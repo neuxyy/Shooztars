aa9edff6386247783f9d1a5e27a008990219190f
neuxyy <neuxyy@gmail.com>
2024-04-08 13:03:33 +0300

    Login and Register added - have to be fixed

--- store/views.py
+++ store/views.py
@@ -1,11 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 import json
 import datetime
+
+
+from .forms import OrderForm, CreateUserForm
 from . utils import cartData, guestOrder
-
-
 from .models import *
+
+def RegisterPage(request):
+    form = CreateUserForm()
+    
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for ' + user)
+            
+            return redirect('login')
+    
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+def LoginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username = username, password = password)   
+        
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+        else:
+            messages.info(request,'Username or password is incorrect')
+            return redirect(request,'login')
+            
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def store(request):
     data = cartData(request)
@@ -78,7 +117,7 @@ def processOrder(request):
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
 
-    if total == order.get_cart_total:
+    if total == order.get_cart_total():
         order.complete = True
     order.save()
 

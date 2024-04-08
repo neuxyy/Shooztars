0153c17950feb0b448e406cb1a9c1311037b2ccb
neuxyy <neuxyy@gmail.com>
2024-04-08 18:01:37 +0300

    Login-Register fixed

--- store/views.py
+++ store/views.py
@@ -117,7 +117,7 @@ def processOrder(request):
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
 
-    if total == order.get_cart_total():
+    if total == order.get_cart_total:
         order.complete = True
     order.save()
 

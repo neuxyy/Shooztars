dbe69ae49b385297a3105836519d7984d3cdb899
neuxyy <neuxyy@gmail.com>
2024-04-10 17:52:30 +0300

    n

--- static/js/lang_change.js
+++ static/js/lang_change.js
@@ -1,9 +1,9 @@
 function changeLanguage(lang) {
     
     if (lang === 'BG') {
-        // Code to switch the website to Bulgarian
+    
     } else if (lang === 'EN') {
-        // Code to switch the website to English
+    
     }
     document.cookie = "django_language=" + lang;
 

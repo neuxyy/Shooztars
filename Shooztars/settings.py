01f1a36d4d3ca2a7c0eebcca9901dcd638838184
neuxyy <neuxyy@gmail.com>
2024-04-11 12:21:39 +0300

    added and env file for sensetive info cleaned some of the .gitignore files and added password change

--- Shooztars/settings.py
+++ Shooztars/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configurations
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('your-email@example.com')
+EMAIL_HOST_PASSWORD = config('your-email-password')
--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls import re_path
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 from.import views
 
 urlpatterns = [
@@ -17,6 +18,10 @@ urlpatterns = [
     path('search/', views.search_filter, name='search'),
     
     
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
     re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
     

1d2861f395e250baa322a25aedd857d0644dec17
neuxyy <neuxyy@gmail.com>
2024-04-15 16:28:44 +0300

    reverted last commit

--- Shooztars/settings.py
+++ Shooztars/settings.py
@@ -143,8 +143,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = config('RAILWAY_VOLUME_MOUNT_PATH')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

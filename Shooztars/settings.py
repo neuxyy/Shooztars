94dac375560e6dbb0f81a2bff5a14fb0ba7df286
neuxyy <neuxyy@gmail.com>
2024-04-16 18:12:25 +0300

    added new dependency

--- Shooztars/settings.py
+++ Shooztars/settings.py
@@ -145,6 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
+#MEDIA_ROOT = config("RAILWAY_VOLUME_MOUNT_PATH")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

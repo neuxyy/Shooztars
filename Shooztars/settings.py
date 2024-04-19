8f0b4b5b32180b044323adf6a48e395655d8601b
neuxyy <neuxyy@gmail.com>
2024-04-19 19:47:24 +0300

    d

--- Shooztars/settings.py
+++ Shooztars/settings.py
@@ -134,7 +134,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
